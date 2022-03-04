482112861809f9e35ce939e5eac3ea1fa0101ac1
ellisonzhao <zhaoqixuan20@qq.com>
2022-03-04 21:55:26 +0800

    增加 No.700 的 cpp 题解

---
+++ leetcode/num_700/solution.cc
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  TreeNode* searchBST(TreeNode* root, int val) {
+    if (root == nullptr || root->val == val) {
+      return root;
+    }
+
+    if (root->val > val) {
+      return searchBST(root->left, val);
+    }
+
+    return searchBST(root->right, val);
+  }
+};
