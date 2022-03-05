0ed35e17853b4496d55737ebaa1f9f41a119d078
ellisonzhao <zhaoqixuan20@qq.com>
2022-03-05 22:32:55 +0800

    增加 No.701 的 cpp 题解

---
+++ leetcode/num_701/solution.cc
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  TreeNode* insertIntoBST(TreeNode* root, int val) {
+    if (root == nullptr) {
+      return new TreeNode(val);
+    }
+
+    if (root->val > val) {
+      root->left = insertIntoBST(root->left, val);
+    }
+    if (root->val < val) {
+      root->right = insertIntoBST(root->right, val);
+    }
+    return root;
+  }
+};
