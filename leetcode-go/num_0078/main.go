8e5fb7b226ec5ee948f4f16d5fcb40c5a751622c
ellisonzhao <ellisonzhao@tencent.com>
2023-02-25 16:51:02 +0800

    feat: 修改「78. 子集」的 Go 题解

--- leetcode-go/num_0078/main.go
+++ leetcode-go/num_0078/main.go
@@ -1,55 +1,68 @@
 package main
 
-// func subsets(nums []int) (ans [][]int) {
-//	n := len(nums)
-//	for mask := 0; mask < (1 << n); mask++ {
-//		var set []int
-//		for i, v := range nums {
-//			if ((mask >> i) & 1) > 0 {
-//				set = append(set, v)
-//			}
-//		}
-//		// tmp := []int{}
-//		ans = append(ans, append([]int{}, set...))
-//	}
-//	return
+import (
+	"fmt"
+)
+
+// func subsets(nums []int) [][]int {
+// 	var ans [][]int
+// 	n := len(nums)
+// 	for mask := 0; mask < (1 << n); mask++ {
+// 		var subset []int
+// 		for i, v := range nums {
+// 			// 二进制位为 1 表示「在子集中」
+// 			if ((mask >> i) & 1) > 0 {
+// 				subset = append(subset, v)
+// 			}
+// 		}
+// 		ans = append(ans, subset)
+// 	}
+// 	return ans
 // }
 
-// func subsets(nums []int) (ans [][]int) {
-//	var set []int
-//	var dfs func(int)
-//	dfs = func(cur int) {
-//		if cur == len(nums) {
-//			ans = append(ans, append([]int(nil), set...))
-//			return
-//		}
-//		set = append(set, nums[cur])
-//		dfs(cur + 1)
-//		set = set[:len(set)-1]
-//		dfs(cur + 1)
-//	}
-//	dfs(0)
-//	return
+// func subsets(nums []int) [][]int {
+// 	var (
+// 		ans [][]int
+// 		dfs func(int, []int)
+// 	)
+// 	dfs = func(idx int, tmp []int) {
+// 		if idx == len(nums) {
+// 			ans = append(ans, append([]int(nil), tmp...))
+// 			return
+// 		}
+// 		dfs(idx+1, tmp)
+// 		tmp = append(tmp, nums[idx])
+// 		dfs(idx+1, tmp)
+// 		tmp = tmp[:len(tmp)-1]
+// 	}
+// 	dfs(0, []int(nil))
+// 	return ans
 // }
 
-func subsets(nums []int) (results [][]int) {
-	var temp []int
-	var dfs func(int, []int)
-	dfs = func(idx int, temp []int) {
+func subsets(nums []int) [][]int {
+	var (
+		ans [][]int
+		dfs func(int, []int)
+	)
+	dfs = func(idx int, tmp []int) {
 		if idx == len(nums)+1 {
 			return
 		}
-
-		results = append(results, append([]int{}, temp...))
-
+		ans = append(ans, append([]int(nil), tmp...))
 		for i := idx; i < len(nums); i++ {
-			temp = append(temp, nums[idx])
-			dfs(i+1, temp)
-			temp = temp[:len(temp)-1]
+			tmp = append(tmp, nums[i])
+			dfs(i+1, tmp)
+			tmp = tmp[:len(tmp)-1]
 		}
 	}
 
-	dfs(0, temp)
+	dfs(0, []int{})
 
-	return
+	return ans
+}
+
+func main() {
+	nums := []int{9, 0, 3, 5, 7}
+	ans := subsets(nums)
+	fmt.Println(ans)
 }
