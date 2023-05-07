62723f2fac23df4729d9504a1badc6a2c91d2f8e
ellisonzhao <ellisonzhao@tencent.com>
2023-05-07 14:53:44 +0800

    feat: 增加「48. 旋转图像」的 Go 题解

---
+++ leetcode-go/num_0048/main.go
@@ -0,0 +1,15 @@
+package num_0048
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+		}
+	}
+}
