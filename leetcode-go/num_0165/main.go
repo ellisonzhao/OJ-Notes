d98aa63aee9e638fa8d662e45c0f990a94f1f59b
ellisonzhao <ellisonzhao@tencent.com>
2023-07-30 16:47:58 +0800

    feat: 增加「165. 比较版本号」的 Go 题解

--- leetcode-go/num_0165/main.go
+++ leetcode-go/num_0165/main.go
@@ -45,6 +45,33 @@ func strToInt(str string) int {
 	return num
 }
 
+func compareVersion1(version1 string, version2 string) int {
+	var num1, num2 int
+	len1, len2 := len(version1), len(version2)
+	for i, j := 0, 0; i < len1 || j < len2; {
+		num1, num2 = 0, 0
+		for i < len1 && version1[i] != '.' {
+			num1 = num1*10 + int(version1[i]-'0')
+			i++
+		}
+		for j < len2 && version2[j] != '.' {
+			num2 = num2*10 + int(version2[j]-'0')
+			j++
+		}
+
+		if num1 > num2 {
+			return 1
+		} else if num1 < num2 {
+			return -1
+		} else {
+			i++
+			j++
+		}
+	}
+
+	return 0
+}
+
 func main() {
 	version1 := "1.01"
 	version2 := "1.001"
