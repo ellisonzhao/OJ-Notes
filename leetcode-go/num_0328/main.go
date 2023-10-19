61f52ae903c3b6b4b9241a46c74e823efb9c8991
ellisonzhao <ellisonzhao@tencent.com>
2023-10-19 20:21:20 +0800

    feat: 增加「328. 奇偶链表」的 Go 题解

---
+++ leetcode-go/num_0328/main.go
@@ -0,0 +1,31 @@
+package num_0328
+
+import (
+	"github.com/ellisonzhao/OJ-Solution/leetcode-go/algorithm"
+)
+
+type ListNode = algorithm.ListNode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	odd := head
+	evenHead, even := head.Next, head.Next
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+
+	return head
+}
