d3933ac5a46cfa4002c932e65cd3e8b7ec23042b
ellisonzhao <ellisonzhao@tencent.com>
2023-11-05 23:29:39 +0800

    feat: 增加【402. 移掉 K 位数字】的 Go 题解

--- leetcode-go/num_0402/main.go
+++ leetcode-go/num_0402/main.go
@@ -5,22 +5,17 @@ import (
 )
 
 func removeKdigits(num string, k int) string {
-	if len(num) <= k {
-		return ""
-	}
-
 	var stack []byte
 	for i := range num {
 		digit := num[i]
-		// 例如栈内是 14，此时遇到 3，显然 13 是比 14 更小的数，因此更新栈顶元素
+		// 模拟删除 k 个数字
 		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
 			stack = stack[:len(stack)-1]
 			k--
 		}
-		// 维护单调不减的数字序列
 		stack = append(stack, digit)
 	}
-	// 数字序列已经是升序，取前 n-k 个构成的结果最小，即去掉尾部 k 个数字
+	// 如果 k>0 说明还要删掉剩余的 k 个数字
 	stack = stack[:len(stack)-k]
 	// 去掉前导 0
 	ans := strings.TrimLeft(string(stack), "0")
